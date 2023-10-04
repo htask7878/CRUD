eacfa4919d73f1199fe6aca1d909f0c08de44c49
H T Beladiya <pdfile005@gmail.com>
2023-10-04 13:50:26 +0530

    Add project files.

---
+++ CRUD/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index(crud_model dbm, int a = 0)
+        {
+            DataSet ds = dbm.get_data();
+            ViewBag.user_data = ds.Tables[0];
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(crud_model dbm)
+        {
+            string name = dbm.name;
+            string email = dbm.email;
+            string password = dbm.password;
+
+            dbm.insert_data(name, email, password);
+
+            return RedirectToAction("Index");   // get
+        }
+
+        [HttpGet]
+        public IActionResult delete(crud_model d,int id=0)
+        { 
+            d.delete_data(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult update_data(crud_model u, int u_id)
+        {
+            DataSet ds = u.select_update_data(u_id);
+            ViewBag.u_data = ds.Tables[0];
+
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult update_data(crud_model u, int u_id,int a=0)
+        {
+            string name = u.name;
+            string email = u.email;
+            string password = u.password;
+
+            u.update_data(name, email, password, u_id);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CRUD/Models/crud_model.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUD.Models
+{
+    public class crud_model
+    {
+        SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Database=CRUD;User Id=sa;pwd=cdmi123");
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public int insert_data(string name,string email,string password)
+        {
+            SqlCommand cmd = new SqlCommand("insert into [dbo].[user](name,email,password)values('" + name + "','" + email + "','" + password + "')", con);
+            con.Open();
+            return cmd.ExecuteNonQuery();
+        }
+
+        public DataSet get_data()
+        {
+            SqlCommand cmd = new SqlCommand("select * from [dbo].[user]",con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;  
+        }
+
+        public int delete_data(int id)
+        {
+            SqlCommand cmd = new SqlCommand("delete from [dbo].[user] where id='" + id + "'", con);
+            con.Open();
+            return cmd.ExecuteNonQuery();
+        }
+
+        public DataSet select_update_data(int id)
+        {
+            SqlCommand cmd = new SqlCommand("select * from [dbo].[user] where id='"+id+"'",con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        public int update_data(string name,string email,string password,int id)
+        {
+            SqlCommand cmd = new SqlCommand("update [dbo].[user] set name='" + name + "',email='" + email + "',password='" + password + "' where id='" + id + "'", con);
+            con.Open();
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
